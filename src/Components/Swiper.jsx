f3a4c45be7cdc8ed3772ed4e667c8637aeee7286
AlaminIslamOnu <alamin.ndc.17@gmail.com>
2025-05-22 17:19:41 +0600

    reduce home page img

--- src/Components/Swiper.jsx
+++ src/Components/Swiper.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import firstImg from '../assets/first.jpg'
-import secondImg from '../assets/second.jpg'
-import thirdImg from '../assets/trird.jpg' 
+import React from "react";
+import firstImg from "../assets/first.jpg";
+import secondImg from "../assets/second.jpg";
+import thirdImg from "../assets/trird.jpg";
 const Swiper = () => {
   return (
     <div className="carousel w-full relative">
@@ -11,15 +11,21 @@ const Swiper = () => {
           className="w-full h-48 md:h-72 lg:h-96 object-cover"
           alt="Slide 1"
         />
-        <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 
+        <div
+          className="absolute bottom-10 left-1/2 transform -translate-x-1/2 
              bg-gradient-to-r from-green-700 to-transparent 
              text-white px-6 py-3 rounded-md text-lg font-semibold max-w-md text-center
-             grid place-items-centere">
+             grid place-items-centere"
+        >
           Green is not just a color, it’s a lifestyle.
         </div>
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide4" className="btn btn-circle">❮</a>
-          <a href="#slide2" className="btn btn-circle">❯</a>
+          <a href="#slide3" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
         </div>
       </div>
 
@@ -29,15 +35,21 @@ const Swiper = () => {
           className="w-full h-48 md:h-72 lg:h-96 object-cover"
           alt="Slide 2"
         />
-        <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 
+        <div
+          className="absolute bottom-10 left-1/2 transform -translate-x-1/2 
              bg-gradient-to-r from-green-700 to-transparent 
              text-white px-6 py-3 rounded-md text-lg font-semibold max-w-md text-center
-             grid place-items-centere">
+             grid place-items-centere"
+        >
           Nurture nature, and it will nurture you back.
         </div>
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide1" className="btn btn-circle">❮</a>
-          <a href="#slide3" className="btn btn-circle">❯</a>
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
         </div>
       </div>
 
@@ -47,19 +59,23 @@ const Swiper = () => {
           className="w-full h-48 md:h-72 lg:h-96 object-cover"
           alt="Slide 3"
         />
-        <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 
+        <div
+          className="absolute bottom-10 left-1/2 transform -translate-x-1/2 
              bg-gradient-to-r from-green-700 to-transparent 
              text-white px-6 py-3 rounded-md text-lg font-semibold max-w-md text-center
-             grid place-items-centere">
+             grid place-items-centere"
+        >
           Transform your space with the calm of green leaves.
         </div>
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide2" className="btn btn-circle">❮</a>
-          <a href="#slide4" className="btn btn-circle">❯</a>
+          <a href="#slide2" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-circle">
+            ❯
+          </a>
         </div>
       </div>
-
-      
     </div>
   );
 };
