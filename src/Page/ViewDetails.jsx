9f20b2d73af5afb7af69473d285c43f5c02a0e05
AlaminIslamOnu <alamin.ndc.17@gmail.com>
2025-05-23 19:32:45 +0600

    design view details page

--- src/Page/ViewDetails.jsx
+++ src/Page/ViewDetails.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import { useLoaderData } from 'react-router';
+import React from "react";
+import { useLoaderData } from "react-router";
 
 const ViewDetails = () => {
-    const datas= useLoaderData()
-    console.log(datas);
-    return (
-        <div>
-             THis is viewDetils       
+  const datas = useLoaderData();
+  console.log(datas);
+  const {image,plantName,category,description,wateringFrequency,healthStatus}= datas
+  return (
+    <div className="hero">
+      <div className="hero-content flex-col lg:flex-row ">
+        <img
+          src={image}
+          className="max-w-sm rounded-lg shadow-2xl"
+        />
+        <div className="space-y-4">
+          <h1 className="text-5xl font-bold">{plantName}</h1>
+          <p className="">
+            Category : {category}
+            
+          </p>
+          <p>Watering Frequency: {wateringFrequency}</p>
+          <p className="font-bold">Health Status: {healthStatus}</p>
+          <p>Description: {description}</p>
+          
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ViewDetails;
